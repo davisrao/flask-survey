65ec6e12e5c72a7954c8dbe0ab69cfa9d45680d3
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-09-09 12:37:46 -0700

    updated step 2

--- app.py
+++ app.py
@@ -9,11 +9,22 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-
+# We need to post the answers from the survey questions into the responses list
 
 @app.get('/')
 def show_survey():
+    """ Returns a render template of survey start """
     return render_template("survey_start.html", 
                             title=survey.title, 
                             instructions=survey.instructions, 
                             questions=survey.questions)
+
+@app.post("/begin")
+def 
+""" It'll redirect you to the start of the question/0"""
+
+@app.get("/question/<question_index>")
+def show_questions(question_index):
+    """ Returns a render template of the question """
+    question_index = int(request.args["question_index"])
+    return render_template("question.html", question=survey.questions[question_index])
